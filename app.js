9b854253feffdffc03cd6fdfb82eb890220a77da
bwapes <benwaples@gmail.com>
2020-07-13 15:09:15 -0700

    test is passing

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 // import functions and grab DOM elements
+const doorButton = document.querySelector('#store-door');
 
 // initialize state
 
 // set event listeners to update state and DOM 
+doorButton.addEventListener('click', () => {
+    
+})
---
+++ productFolder/rowingInventory.js
@@ -0,0 +1,13 @@
+//create 5 rowing items and the data structure, then export them to productApp
+
+const product = [
+    {
+        id: 'oar',
+        name: 'Port oars',
+        image: 'photos/oartulips.jpg',
+        description: 'This is designed to go faster than starboard oars no matter what',
+        category: 'equipment',
+        price: 20,
+        cost: 19.99
+    },
+]
