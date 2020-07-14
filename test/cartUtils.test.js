e7ca08fcbada8a17960c9475b87bffb66f722177
bwapes <benwaples@gmail.com>
2020-07-14 14:31:18 -0700

    findById passing TDD

--- test/cartUtils.test.js
+++ test/cartUtils.test.js
@@ -24,3 +24,48 @@ test('receive an array of objects and create an html tr element from it', (expec
     // Make assertions about what is expected versus the actual result
     expect.equal(html, expected);
 });
+
+test('it should take in an id and an array and return the matching item', (assert) => {
+     //Arrange
+    // Set up your parameters and expectations
+
+    //test hardcoded data
+    const spot = {
+        id: 'spot',
+        type: 'doggy',
+        weight: 5
+    };
+    
+    const douglas = {
+        id: 'douglas',
+        type: 'doggy',
+        weight: 12
+    };
+    
+    const jumpy = {
+        id: 'jumpy',
+        type: 'froggy',
+        weight: 1
+    };
+    
+    const myArray = [
+        spot,
+        douglas,
+        jumpy
+    ];
+
+    const myId1 = 'spot';
+    const myId2 = 'jumpy';
+    const expected1 = spot;
+    const expected2 = jumpy;
+
+    //Act 
+    // Call the function you're testing and set the result to a const
+    const actual1 = findById(myArray, myId1);
+    const actual2 = findById(myArray, myId2);
+    
+    //Assert
+    // Make assertions about what is expected versus the actual result
+    assert.equal(actual1, expected1);
+    assert.equal(actual2, expected2);
+});
