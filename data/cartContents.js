962efbf0fde4d19bf9a39185517a9f0727bd4a2c
bwapes <benwaples@gmail.com>
2020-07-14 13:32:26 -0700

    just kidding now hard coded the cart

--- data/cartContents.js
+++ data/cartContents.js
@@ -1,15 +1,20 @@
 const cart = [{
-    id: 'apple',
-    quantity: 2
-}, {
-    id: 'banana',
-    quantity: 5
-}, {
-    id: 'mango',
+    id: '0aRP0rt',
     quantity: 1
 }, {
-    id: 'orange',
-    quantity: 10
-}];
+    id: '0Ar57aR',
+    quantity: 3
+}, {
+    id: 'p0c0ck13',
+    quantity: 1
+}, {
+    id: 'tr0uz35z4d00ds',
+    quantity: 2
+}, {
+    id: 'tr0uz35z4L4d13s',
+    quantity: 2
+}
+];
+
 
 export default cart;
