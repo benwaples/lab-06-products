3266221296ef6e1ed1058b0b4cfa96c486f2927e
bwapes <benwaples@gmail.com>
2020-07-14 22:18:50 -0700

    tests are passing again

--- test/createLineItem.test.js
+++ test/createLineItem.test.js
@@ -1,5 +1,5 @@
 import items from '../data/rowingInventory.js';
-import { createLineItem, findById } from '../cart/cartUtils.js';
+import { createLineItem } from '../cart/cartUtils.js';
 
 const test = QUnit.test;
 
@@ -11,13 +11,12 @@ test('receive an array of objects and create an html tr element from it', (expec
         id: '0aRP0rt',
         quantity: 1
     };
-    const oar = findById(items, cartItem.id);
     
     const expected = '<tr><td>Port oars</td><td class="qty">1</td><td>$20.00</td><td>$20.00</td></tr>';
     
     //Act 
     // Call the function you're testing and set the result to a const
-    const dom = createLineItem(cartItem, oar);
+    const dom = createLineItem(cartItem, items);
     const html = dom.outerHTML;
 
     //Expect
