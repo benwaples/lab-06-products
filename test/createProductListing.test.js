c43cf8d83bb2d7ddae1c1d532255b20585996403
bwapes <benwaples@gmail.com>
2020-07-15 16:19:45 -0700

    test is passing with updated render product listing

--- test/createProductListing.test.js
+++ test/createProductListing.test.js
@@ -6,7 +6,7 @@ test('receive an array of objects and create an html li element from it', (expec
     //Arrange
     // Set up your arguments and expectations
     const oar = {
-        id: 'oar',
+        id: '0aRP0rt',
         name: 'Port oars',
         image: 'photos/oartulips.jpg',
         description: 'This is designed to go faster than starboard oars no matter what',
@@ -15,7 +15,7 @@ test('receive an array of objects and create an html li element from it', (expec
         cost: 19.99
     };
     
-    const expected = '<li class="equipment" title="This is designed to go faster than starboard oars no matter what"><h3>Port oars</h3><img src="photos/oartulips.jpg" alt="Port oars image"><p class="price">$20.00<button value="oar">Add</button></p><p class="description">This is designed to go faster than starboard oars no matter what</p></li>';
+    const expected = '<li class="equipment" title="This is designed to go faster than starboard oars no matter what"><h3>Port oars</h3><img src="photos/oartulips.jpg" alt="Port oars image"><p class="price">$20.00<select id="qtySelector0aRP0rt"><option value="1">1</option><option value="2">2</option><option value="3">3</option><option value="4">4</option></select><button value="0aRP0rt">Add</button></p><p class="description">This is designed to go faster than starboard oars no matter what</p></li>';
     
     //Act 
     // Call the function you're testing and set the result to a const
